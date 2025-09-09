0f59f0b493dbde37fb6d216aee47217c7024d898
Olek Raymond <123381967+OlekRaymond@users.noreply.github.com>
2025-09-09 17:56:14 +0100

    Adds files

---
+++ BuildSlides.py
@@ -0,0 +1,328 @@
+
+from typing import Protocol, Callable, LiteralString, override, Final
+from dataclasses import dataclass
+import subprocess
+from os import getenv as get_env
+import os
+import re
+
+# parse slides
+# if code block:
+#   if python:
+#       try exec; green; expect: red
+#   if C++:
+#       add to template.cpp;
+#       compile
+#       if compiles: green; else red;
+
+
+markdown_to_parse = """
+```C++
+/* compiles */
+```
+```C++
+does not compile
+```
+```Cpp
+/* compiles */
+```
+```Cpp
+does not compile
+```
+# heading one
+## heading two
+---
+something else
+```py
+print(1)
+```
+```Python
+assert false
+```
+```py
+exit(2)
+```
+```Py
+exit("good bye")
+```
+"""
+# parsed_markdown = parser.parse(markdown_to_parse)
+
+# def filter_code(markdown:list["markdown_it.token.Token"]):
+#     for token in markdown:
+#         if token.tag == "code":
+#             yield token
+#         if token.children:
+#             child_code = filter_code(token.children)
+#             yield from child_code
+
+@dataclass
+class CompileResult:
+    compiler_output: str
+    return_code: int
+    @property
+    def compiles(self): return self.return_code == 0
+
+@dataclass
+class RunResult:
+    run_output: str
+    return_code: int
+    @property
+    def runs(self): return self.return_code == 0
+
+@dataclass
+class LinkResult:
+    link_output: str
+    return_code: int
+    @property
+    def links(self): return self.return_code == 0
+
+@dataclass
+class CodeResult:
+    compile_result: CompileResult | None
+    # link_result: LinkResult | None
+    run_result: RunResult | None
+    @property
+    def compiles(self) -> bool:
+        if self.compile_result is not None: return self.compile_result.compiles
+        if self.run_result is not None: return self.run_result.runs
+        return False # ?
+    @property
+    def runs(self) -> bool:
+        if self.run_result is not None: return self.run_result.runs
+        return False
+
+class RuntimeLanguage:
+    def __init__(self, string:str) -> None:
+        if len(string) > 10 or ' ' in string:
+            raise RuntimeError("Language did not fill appropriate requirements")
+        self._value:str = string.lower()
+
+    def __str__(self) -> str:
+        return self._value
+    
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, RuntimeLanguage): return False
+        return value._value == self._value
+    
+    def __hash__(self) -> int:
+        return self._value.__hash__()
+
+type Handler  = Callable[[str], CodeResult]
+# type RuntimeLanguage = str
+type Language = LiteralString | RuntimeLanguage
+
+class CodeHandlerRegistry(Protocol):
+    def handle_code(self, language:RuntimeLanguage, code:str) -> CodeResult: ...
+    def add_language(self, language: LiteralString, handler: Handler) -> "CodeHandlerRegistry": ...
+
+
+class DefaultHandler(CodeHandlerRegistry):
+    def __init__(self):
+        self.registry: dict[RuntimeLanguage, Handler] = {}
+    @override
+    def add_language(self, language:LiteralString, handler:Handler) -> "DefaultHandler":
+        self.registry.update({RuntimeLanguage(str(language)): handler})
+        return self
+
+    @override
+    def handle_code(self, language:RuntimeLanguage, code:str) -> CodeResult:
+        assert isinstance(language, RuntimeLanguage)
+        handler = self.registry.get(language)
+        if not handler:
+            raise RuntimeError(
+                f"Cannot handle code for this language: {language}\n"
+                f"Available languages are: {', '.join((str(k) for k in self.registry.keys()))}"
+                )
+        return handler(code)
+
+def handle_code(language: Language, code:str, handler_registry: CodeHandlerRegistry | None = None):
+    handler: CodeHandlerRegistry = handle_code._default_handler if handler_registry is None else handler_registry
+    return handler.handle_code(language, code)
+
+handle_code._default_handler = DefaultHandler()
+
+def try_executable(exe_path:str) -> bool:
+    res = subprocess.run((exe_path, "--version"), stdout=subprocess.DEVNULL)
+    return res.returncode == 0
+
+def find_executable(exe_name:str) -> str | None:
+    res = subprocess.run(("whereis", exe_name), stdout=subprocess.PIPE, check=True)
+    if len(res.stdout) > len(exe_name) + 3:
+        return str(res.stdout).replace(exe_name, "").replace(":", "").split()[0]
+
+def _get_cpp_compiler() -> str:
+    compiler = get_env("CXX")
+    if compiler is not None: return compiler
+    
+    if try_executable("g++"): return "g++"
+    if try_executable("clang++"): return "clang++"
+
+    if compiler_path := find_executable("g++"): return compiler_path
+    if compiler_path := find_executable("clang++"): return compiler_path
+    raise RuntimeError("could not find C++ compiler")
+
+_CPP_COMPILER:str = _get_cpp_compiler()
+
+def handle_cpp(code:str, debug_value:str |None = None) -> CodeResult:
+    handle_cpp._counter += 1
+    file_name = debug_value if debug_value is not None else f"build/main_{handle_cpp._counter}"
+    source_file_name = f"{file_name}.cpp"
+    # obj_file_name = f"{file_name}.obj"
+    exe_file_name = f"{file_name}"
+    with open("template.cpp.in", "r") as template, open(source_file_name, "w") as output:
+        output.write(template.read().replace("@input@", code))
+    # if compile only use "-c" in the following command, we test compiling and linking for now 
+    res = subprocess.run((_CPP_COMPILER, "-o" f"{exe_file_name}" , source_file_name), stderr=subprocess.PIPE)
+    compile_result = CompileResult(str(res.stderr), res.returncode )
+    if res.returncode != 0: return CodeResult(compile_result=compile_result, run_result=None)
+    # breakpoint()
+    res = subprocess.run((f"./{exe_file_name}"), stderr=subprocess.PIPE)
+    run_result = RunResult(str(res.stderr), res.returncode)
+    return CodeResult(run_result=run_result, compile_result=compile_result)
+
+handle_cpp._counter = 0
+
+def handle_python(code:str) -> CodeResult:
+    print_result:str = ""
+    exit_code:int = 0
+    exit_str:str = ""
+    def mock_print(*value:object):
+        nonlocal print_result
+        print_result += str(value[0])
+    def mock_exit(value:int|str):
+        nonlocal exit_code
+        nonlocal exit_str
+        if isinstance(value, int): 
+            exit_code = value
+            return
+        exit_code = 1
+        exit_str = value
+
+    try:
+        exec(code, {"print": mock_print, "exit":mock_exit})
+        return CodeResult(None, RunResult(print_result + exit_str, exit_code))
+    except Exception as e:
+        return CodeResult(None, RunResult(print_result + e.__repr__(), 1))
+
+handle_code._default_handler.add_language("Cpp", handle_cpp)
+handle_code._default_handler.add_language("C++", handle_cpp)
+handle_code._default_handler.add_language("Python", handle_python)
+handle_code._default_handler.add_language("py", handle_python)
+
+def result_to_string(result:CodeResult, wants:str) -> str:
+    wants = wants.lower()
+    wants_options = ("compile", "run")
+    if wants not in wants_options:
+        raise RuntimeError(f"Unknown wants value: {wants}, expected one of {', '.join(wants_options)}")
+    
+    if wants == "compile": return "rayjs-compiling" if result.compiles else "reyjs-not-compiling"
+    if wants == "run": return "rayjs-running" if result.runs else "reyjs-erroring"
+
+type Code = str
+type Markdown = str
+
+_FIND_CODE_PATTERN = r"(?:^```)(?P<lang>[A-Za-z+]{2,10})(?:.{0,20})$(?:\r?\n)(?P<code>(?:[^\`]){3,}?)(?:```$\n)(?:\<\!\-\- \.element: class=\")(?P<outclass>[A-Za-z0-9\-_]*?)(?:\")(?:\s?wants=\")(?P<outwants>[A-Za-z0-9\-_]*?)(?:\" \-\-\>)"
+_COMPILED_REGEX = re.compile(_FIND_CODE_PATTERN, re.MULTILINE)
+
+def for_each_code_block(input:Markdown, code_handler:Callable[[RuntimeLanguage, Code], CodeResult]):
+    def on_match(full_match:re.Match[str]) -> str:
+        groups = full_match.groupdict()
+        reconstructed = full_match.string[full_match.start():full_match.end()]
+        language = groups["lang"]
+        if groups["outwants"].lower() == "nothing":
+            print("Did not run code for language: ", language, " because wants was ", groups["outwants"])
+            return reconstructed
+        language = RuntimeLanguage(language)
+        code :Code = groups["code"]
+        code_result = code_handler(language, code)
+        reconstructed = reconstructed.replace(groups["outclass"], result_to_string(code_result, groups["outwants"]))
+        return reconstructed
+    return _COMPILED_REGEX.sub(on_match, input)
+
+def template_file_setup(base_template_file:str, reveal_js_path:str) -> str:
+    with open(base_template_file, "r") as template_file:
+        return template_file.read().replace("@__REVEAL_JS_PATH__@", reveal_js_path)
+
+def fill_output_template(input_markdown:str, template_file:str, output_file_name:str) -> None:
+    with open(output_file_name, "w") as out_file:
+        out_file.write(template_file.replace("@__MARKDOWN INPUT__@", input_markdown))
+
+def _get_git_path() -> str:
+    if try_executable("git"): return "git"
+    if (path_ := find_executable("git")) is not None: return path_
+    raise RuntimeError("Could not find git executable")
+
+_GIT_PATH = _get_git_path()
+
+def _clone_reveal_js(*, 
+        destination_folder:str = "build/reveal_js",
+        version_tag:str = "5.2.1",
+        repo_url:str = "https://github.com/hakimel/reveal.js.git"
+    ) -> str:
+    dest_dir = destination_folder if destination_folder.endswith("/") else destination_folder + "/"
+    if os.path.exists(destination_folder):
+        return dest_dir
+    clone = subprocess.run(
+        (_GIT_PATH, "clone", "-b", version_tag, "-q", "--depth", "1", "--single-branch", repo_url , destination_folder),
+        stderr=subprocess.PIPE
+    )
+    if clone.returncode == 0:
+        return dest_dir
+    print(f"Could not clone reveal.js, got {clone.stderr}, using CDN instead")
+    return f"https://cdnjs.cloudflare.com/ajax/libs/reveal.js/{version_tag}/"
+
+_REVEAL_JS_PATH:Final = _clone_reveal_js()
+
+def create_markdown_file(input_file_name:str, *,
+        template_file_name:str = "TemplateSlides.html.in",
+        output_file_name:str = "index.html",
+        reveal_js_path:str = _REVEAL_JS_PATH
+    ) -> None:
+    with open(input_file_name, "r") as in_file:
+        markdown_file_data = in_file.read()
+        new_markdown_data = for_each_code_block(markdown_file_data, handle_code)
+    template_half_filled = template_file_setup(template_file_name, reveal_js_path)
+    fill_output_template(new_markdown_data, template_half_filled, output_file_name)
+
+_HELP = (
+"""
+Create slides from markdown file with code blocks that can be compiled and executed.
+Usage: BuildSlides.py [options] <input_markdown_files>
+Options:
+-t --template <template_file>
+    Specify the template file to use. Default is TemplateSlides.html.in
+-o --output <output_file>
+    Specify the output file name. Default is index.html
+input_markdown_files
+    One or more markdown files to process.
+-h --help
+    Show this help message.
+"""
+)
+
+def main() -> None:
+    import argparse
+    arg_parser = argparse.ArgumentParser(description="Create slides from markdown file with code blocks that can be compiled and executed.", add_help=False)
+    arg_parser.add_argument("input_files", metavar="input_markdown_files", type=str, nargs="+", help="One or more markdown files to process.")
+    arg_parser.add_argument("-t", "--template", type=str, default="TemplateSlides.html.in", help="Specify the template file to use. Default is TemplateSlides.html.in")
+    arg_parser.add_argument("-o", "--output", type=str, default="index.html", help="Specify the output file name. Default is index.html")
+    arg_parser.add_argument("-r", "--reveal-js-path", type=str, default=_REVEAL_JS_PATH, help="Path to reveal.js folder or CDN URL. Default is to clone reveal.js repo.")
+    arg_parser.add_argument("-h", "--help", action="store_true", help="Show this help message.") 
+    args = arg_parser.parse_args()
+    if args.help:
+        print(_HELP)
+        exit(1)
+    # Allow *.md, use set to only do each file once
+    # print(f"{args.input_files=}")
+    input_files:set[str] = {f for f in args.input_files if os.path.isfile(f) and f.endswith(".md")}
+    # print(input_files)
+    
+    for input_file in input_files:
+        output_file = args.output if len(input_files) == 1 else input_file.rsplit(".", 1)[0] + ".html"
+        print(f"Processing {input_file} to {output_file} using template {args.template}")
+        create_markdown_file(input_file, template_file_name=args.template, output_file_name=output_file, reveal_js_path=args.reveal_js_path)
+
+if __name__ == "__main__":
+    main()
+
---
+++ YouAlreadyKnowC++.md
@@ -0,0 +1,395 @@
+
+# You already know C++
+
+C is bad. The best and worst part of C++ is it's compatibility with C.
+
+[//]: # (Vertical slide)
+
+This means we inherit the best parts of C and the worst.
+
+However C++ is good and offers the high level, safe alternatives we've come to expect from languages.
+I'll show you what these are, where to find them and how C++ is simpler than you probably gave it credit for.
+
+---
+
+## C is evil
+
+In C the programmer is required to keep track of all heap storage, i.e.:
+- Before usage size must be allocated
+- This size cannot change
+- Must be freed when done with
+- Cannot be used again after free
+
+Modern languages handle all this for us, C++ offers all common data structures without need to mess with memory.
+
+---
+
+## How to be safe
+
+Use the standard library.
+
+[//]: # (Vertical slide)
+
+Most common problems people have can be solved by using the standard library:
+- Buffer overflow? Use `std::vector` and `std::string`.
+- Use after free? Use a vector `std::unique_ptr` or `std::shared_ptr`.
+- Access out of bounds? Use bound checked lookups like `.at(size_t)`
+- Iterating through a container? use `for (auto item : container)`
+
+[//]: # (Vertical slide)
+
+The vast majority of unsafe code does things in a C style rather than a C++ style. For example it is unlikely that a file handle would be leaked with C++'s `std::ostream` and much more likely with C's `fopen`.
+
+---
+
+## Still struggling? WWPD
+
+**W**hat **W**ould **P**ython **D**o?
+
+I think python and C++ are very similar and I think good C++ should resemble python.
+
+For example our python code for creating a list of size 10 containing 0 (`ints`):
+
+```python
+import ctypes
+size_t = ctypes.c_size_t
+int32 = ctypes.c_int32
+size_of_list = 10
+size_of_list_bytes = (32/8) * size_of_list
+libc = ctypes.CDLL("libc_malloc_debug.so")
+data_ptr = libc.malloc(size_of_list_bytes)
+```
+<!-- .element: class="unknown" wants="run" -->
+
+[//]: # (Vertical slide)
+
+No?
+
+You don't write python like that?
+
+Then why write C++ like that?
+
+[//]: # (Vertical slide)
+
+- I need an array `A` with 10 `ints` inside, all 0?
+
+```py
+A :list[int] = [0] * 10;
+```
+```C++
+std::array<int, 10> A{0};
+```
+
+[//]: # (Vertical slide)
+
+- Okay but I need it to be resizable.
+```py
+A :list[int] = [0] * 10;
+```
+```C++
+std::vector<int> A(10, 0);
+```
+
+[//]: # (Vertical slide)
+
+- Cool now how do I iterate through it?
+```py
+for a in A: code(a)
+```
+```C++
+for (auto a : A) { code(a); }
+```
+<sub><sup>Where `auto` is "any type, IDC"</sup></sub>
+
+[//]: # (Vertical slide)
+
+- How do I delete the memory so I don't leak it?
+
+```py
+# nothing
+```
+<sub><sup>(Garbage collector handles it)</sup></sub>
+
+```C++
+/* nothing */
+```
+<sub><sup>(Item leaves the stack and destructor handles it)</sup></sub>
+
+---
+
+## I can't use the standard library
+
+Usually because "my input doesn't allow it".
+
+<sub><sup>There are other reasons, some valid, but advice is similar regardless.</sup></sub>
+
+[//]: # (Vertical slide)
+
+### Bad input
+
+If you are given a char pointer (`char*`) rather than a string (also known as a CString) and not something like `std::string` you should get it into something "known safe" as soon as possible.
+
+For our purpose we will use `std::string`.
+
+This means you'll need to check the length of it.
+
+[//]: # (Vertical slide)
+
+In C the length of a string is from the first character to the first string termination character (`\0`).
+
+So to check the length you have to loop through it.
+
+Checking each character to see if it is `\0`.
+
+[//]: # (Vertical slide)
+
+A common issue occurs when using:
+```C++
+size_t strlen(const char* str)
+```
+Which will happily go forever and read out of bounds if string termination character is not found.
+
+[//]: # (Vertical slide)
+
+If the length of the buffer (where the string is held) is known we can use that to limit how far we look for the null terminating character with
+```C++
+size_t strnlen_s(const char* str, size_t max_length)
+```
+
+[//]: # (Vertical slide)
+
+Now we know the string is safe and we have the length of the string we can copy it into something less cumbersome to deal with like a `string` or a `string_view` (assuming we know the lifetime of the object).
+
+---
+
+## But UB?
+
+If you've done something wrong the compiler is likely to tell you about it or at least be able to tell you about it.
+
+To be told about more possible things you've done wrong enable more compiler warnings.
+
+[//]: # (Vertical slide)
+
+For example this code compiles with no warnings on gcc but when ran, crashes because of an infinite loop.
+```C++
+template <typename T>
+struct C {
+    void foo() { static_cast<T*>(this)->foo(); }
+};
+
+struct D : C<D> { };
+
+void f(D *d) { d->foo(); }
+
+int main() {
+    D d{};
+    f(&d);
+}
+```
+
+[//]: # (Vertical slide)
+
+We can let gcc know that actually we maybe want to be warned about such silliness with the flag
+```
+-Winfinite-recursion
+```
+or another one that includes it such as
+```
+-Wall
+```
+
+[//]: # (Vertical slide)
+
+This gives us the output:
+```
+<source>: In member function 'void C<T>::foo() [with T = D]':
+<source>:3:10: warning: infinite recursion detected [-Winfinite-recursion]
+    3 |     void foo() { static_cast<T*>(this)->foo(); }
+      |          ^~~
+<source>:3:44: note: recursive call
+    3 |     void foo() { static_cast<T*>(this)->foo(); }
+      |                  ~~~~~~~~~~~~~~~~~~~~~~~~~~^~
+```
+
+[//]: # (Vertical slide)
+
+To force colleagues to listen to these use: `-Werror` or if the code is already filled with warnings turn some into errors with `-Werror=XXX` and hide it in the build script so they can't turn it off.
+
+---
+
+## But UB!
+
+UB is short for undefined behaviour, this is something that the C++ standard (usually intentionally and explicitly) has not specified, usually because it would force a compromise onto the user.
+
+[//]: # (Vertical slide)
+
+For example if I add two numbers and attempt to store the result in a space that is too small to store that number I can:
+[//]: # (Vertical slide)
+1. Store the result in a larger amount of memory, check if it's bigger than max of the small amount of memory, crash or put it in the smaller bit of memory.
+
+i.e. short + short = long; if long > max_short : crash, otherwise cast; return;
+
+[//]: # (Vertical slide)
+2. Use some CPU trickery to set a flag if an overflow has occurred
+
+i.e. short + short = short; if not did_this_work() : throw (or something)
+
+[//]: # (Vertical slide)
+3. Do all maths with dynamically sized storage and grow it if required
+
+(what python does)
+
+[//]: # (Vertical slide)
+4. Do a saturated add
+
+short A + short B = short; if A + B > max_short : return max_short
+
+(but often quicker)
+
+[//]: # (Vertical slide)
+5. Do an overflow
+
+short + short = short; return short % max_short
+
+[//]: # (Vertical slide)
+
+In 1 we slow the code down (a little, mostly through register contention), in 2 we limit compilation to CPUs with this special magic, in 3 we limit ourselves to processors with dynamically resizable memory (no GPUs), 4 and 5 make enforcements on the processor that might benefit one but be terrible for another (cannot mask int maths as float maths on certain GPUs)
+
+[//]: # (Vertical slide)
+
+So the C++ committee made the decision to make no decision.
+
+You can run your code with UB sanitizers, this means that any code that executes UB will cause a crash. In gcc it's two lines.
+
+[//]: # (Vertical slide)
+
+### But Rust!
+
+For this example Rust crashes on debug but overflows on Release, it's effectively the same as C++ but with slower int maths on some GPUs (compromise 5).
+
+---
+
+## BUT UB!!
+
+Still not convinced?
+
+The `constexpr` keyword was introduced in C++11 and runs code at compile time*. Any code ran at compile time cannot execute undefined behaviour, so will result in a compilation failure if attempted. So any code you can test at compile time is guaranteed to not contain undefined behaviour.
+
+[//]: # (Vertical slide)
+
+```C++
+constexpr int add_one(int in) {
+	return in + 1;
+}
+
+constexpr auto i = add_one(std::numeric_limits<int>::max());
+```
+Gives error:
+```
+error: overflow in constant expression [-fpermissive]
+```
+
+[//]: # (Vertical slide)
+
+\* Technically `constexpr` code must run any time before the program runs, so on embedded devices this might be on device start but realistically it's at compile time.
+
+Again the standard is written this way to allow for different processors to handle things differently, for example if I am compiling for some processor that has much more precise float handling, I probably don't want to use the processor I am compiling the code with to do all my maths.
+
+---
+
+## But garbage collection?
+
+---
+
+### Functions:
+We know that any object going into a function as an argument has a lifetime greater than the local scope of the function.
+
+```C++
+void foo(int& in) {
+	// We know that in is always valid in this scope
+	in += 1;
+}
+
+// We know that a is always valid in this scope
+int a = 1;
+foo(a);
+```
+Here `a` goes through `foo` as argument `in`, we know the whole time it's safe.
+
+
+[//]: # (Vertical slide)
+
+Following the above statement, we (and the compiler) know that any local data inside the function we want to end up outside of the local scope must be moved (or copied) to that outer scope. So:
+```C++
+int foo(int& in) {
+	int i = 1;
+	return i; // (free) Implicit move
+}
+```
+
+[//]: # (Vertical slide)
+
+Or a copy if object is not movable
+```C++
+struct NoMove {  NoMove(NoMove&&) = delete; };
+NoMove foo() {
+	return NoMove{}; // Implicit copy (cannot move because we deleted it), (also usually free)
+}
+```
+
+[//]: # (Vertical slide)
+
+If we do something silly like:
+```C++
+int& foo() { return 1; }
+```
+the compiler errors. If we are more determined about our silliness,
+```C++
+int& foo() { int i = 0; return i}
+```
+the compiler warns us:
+```
+warning: reference to local variable 'i' returned [-Wreturn-local-addr]
+   10 | int& foo() { int i = 0; return i; }`
+	  |                                ^
+```
+So naturally, use `-Werror=return-local-addr` (forced in C++26)
+
+---
+
+Leveraging this idea we can create a guideline:
+
+> A function should return an owning object.
+
+[//]: # (Vertical slide)
+
+Though sometimes we want to return references so lets amend this:
+
+> Functions that return a reference should delete the appropriate overloads where the object they are referencing goes out of scope.
+
+[//]: # (Vertical slide)
+
+```C++
+int& GetGreatest(const std::vector<int> input) {
+     int max = 0;
+     size_t index_of_max = 0;
+     for (size_t i = 0; const auto& value : input) {
+         if (max > value) { max = value; index_of_max = i; }
+     }
+     return input[max];
+}
+```
+Returns a reference to an object that was
+
+We can normally see structs or classes (the same thing) as a very similar extension of this concept:
+```C++
+struct IntRefWrapper {
+	IntRefWrapper(int& wrappedInt) : m_wrappedInt{wrappedInt} {}
+	int& m_wrappedInt;
+};
+
+{
+	int a = 1;
+	IntRefWrapper b{a};
+}
+```
